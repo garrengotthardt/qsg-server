8c6c4be83e6a8cc351c10b6b94cf3ad3a037f888
qtotuan <quynh.totuan@gmail.com>
2017-07-28 11:25:11 -0400

    Add auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # post '/login', to: 'auth#create'
-      # get '/me', to: 'auth#show'
+      post '/login', to: 'auth#create'
+      get '/me', to: 'auth#show'
 
       resources :saver_ads
       resources :categories
