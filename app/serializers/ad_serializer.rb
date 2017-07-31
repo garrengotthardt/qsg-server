4c20e889e047c1b8c504f767c9d6c39e197bce79
sarahsweat <sarahsweat@gmail.com>
2017-07-31 16:29:35 -0400

    updated serializer

--- app/serializers/ad_serializer.rb
+++ app/serializers/ad_serializer.rb
@@ -1,8 +1,4 @@
 class AdSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id, :title, :description, :image_url, :price
-=======
-  attributes :id, :title, :description, :price, :location, :image_url
->>>>>>> 24355cd86955b0386961ba922f1e34b744b058d6
+  attributes :id, :title, :description, :price, :location, :image_url, :creator_id
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
 end
