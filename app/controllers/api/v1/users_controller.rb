9225d01350ebdd5266778079cc3d2ff801dd776b
qtotuan <quynh.totuan@gmail.com>
2017-07-31 16:49:28 -0400

    Tweaks

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,22 @@ module Api
         render json: User.all
       end
 
+      def create
+        @user = User.new()
+        @user.first_name = user_params[:first_name]
+        @user.last_name = user_params[:last_name]
+        @user.email = user_params[:email]
+        @user.save
+        # byebug
+        render json: {user: @user, message: "User was created!", status: 201}
+      end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/login', to: 'auth#create'
+      post '/signup', to: 'users#create'
       get '/me', to: 'auth#show'
 
       resources :saver_ads
