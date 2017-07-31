7f07b0427d42527193624c5d7415e1dd8f3024bc
sarahsweat <sarahsweat@gmail.com>
2017-07-31 15:09:11 -0400

    added profile pictures to users.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email
+  attributes :id, :first_name, :last_name, :email, :image_url
   has_many :ads, foreign_key: "creator_id", serializer: AdCreatorSerializer
 end
